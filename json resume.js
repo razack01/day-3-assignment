c06673867cca1d65085997dd807cef69ba072d95
Abdul Razack <75486001+razack01@users.noreply.github.com>
2022-03-03 22:29:13 +0530

    Add files via upload

---
+++ json resume.js
@@ -0,0 +1,26 @@
+var myresume= {
+  "personal details":{
+
+
+  "name":"yourname",
+  "dob":"your birth year",
+  "location": "where you have located",
+  "contactnumber":"yourcontactnumber",
+  "email":"yourpersonalemail",
+  "education":"educational details",
+  "objective":"career objetive"
+},
+ "profiles":{
+  "skills":"skillset you had possesed",
+  "portfolio":"portfolio of yours",
+  "areasofintertest":"webdeveloping,softwaredevelopment",
+  "experience":"how many years you have domain experience?",
+  "projects":"related to web development",
+  "github":"yourgithubprofile",  
+  "linkedIn":"yourlinkedinurl"
+},
+
+
+
+}
+console.log(myresume)
